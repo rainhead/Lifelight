3733f7f5d366ae99470b56e846a59b44a9a511f6
Peter Abrahamsen <rainhead@gmail.com>
2024-10-17 15:00:20 -0700

    Initial Commit

---
+++ Lifelight/LifelightApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifelightApp.swift
+//  Lifelight
+//
+//  Created by Peter Abrahamsen on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct LifelightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LifelightTests/LifelightTests.swift
@@ -0,0 +1,16 @@
+//
+//  LifelightTests.swift
+//  LifelightTests
+//
+//  Created by Peter Abrahamsen on 10/17/24.
+//
+
+import Testing
+
+struct LifelightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
